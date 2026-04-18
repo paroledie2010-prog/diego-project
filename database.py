d4e024611503a06eff6ef2df54144aaf32e1daf8
Boooooo <paroledie2010@gmail.com>
2026-04-18 16:29:03 -0500

    switch to postgresql

--- database.py
+++ database.py
@@ -1,19 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-# Aquí se crea el archivo de base de datos
-SQLALCHEMY_DATABASE_URL = "sqlite:///./frases.db"
+load_dotenv()
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Función para obtener la base de datos
 def get_db():
     db = SessionLocal()
     try:
